38ecd52091768bd629816d3c61d75d188a8cec0d
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-03-30 16:09:30 -0400

    Added Open Source link for GitHub

--- src/App.js
+++ src/App.js
@@ -81,6 +81,7 @@ function App() {
           </div>
         </div>
       </section>
+      <p><a href="https://github.com/ddepierre2/D_Weather_React" target="_blank" rel="noreferrer" class="open-source-info">Open Source</a> code by Dana De Pierre</p>
     </div>
   );
 }
