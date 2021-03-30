6d1c3b6454954ec81c196aa48a1326b4a6c4987b
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-03-30 14:07:03 -0400

    updated the city

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,12 @@
 import React from "react";
 import axios from "axios";
 
-export default function Weather(){
+export default function Weather(props){
 function handleResponse(response){
-alert(`${response.data.main.temp}`);
+alert(`The temperature in ${props.city} is ${response.data.main.temp}°C`);
 }
 let apiKey = "ed8ab9018735ed237ff0af3c6f9509f3"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=${apiKey}&units=metric`
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`
 axios.get(apiUrl).then(handleResponse);
 return(
   <div><h2>Hello</h2></div>
