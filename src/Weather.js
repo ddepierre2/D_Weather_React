61ec8f4e3917ec5e24f6412972ce406d7b9df635
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-03-31 16:17:58 -0400

    Worked on background transparency

--- src/Weather.js
+++ src/Weather.js
@@ -8,6 +8,7 @@ export default function Weather(props) {
   const [city, setCity] = useState (props.defaultCity);
 
 function axiosCall (response) {
+  console.log(response.data)
   setWeatherInfo({
     ready: true,
     city: response.data.name,
@@ -17,11 +18,10 @@ function axiosCall (response) {
     description: response.data.weather[0].description,
     humidity: Math.round(response.data.main.humidity),
     windspeed: Math.round(response.data.wind.speed),
-    sunrise: "6:20",
-    sunset: "19:10"
   })};
 
   function search(){
+  
     const apiKey = "ed8ab9018735ed237ff0af3c6f9509f3"
   let apiUnits = "metric"
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${apiUnits}`
@@ -43,7 +43,6 @@ setCity(event.target.value);
         <form className="location-form" onSubmit={searchCity}>
         <input type="search" placeholder="Enter your city" autoComplete="off" onChange={handleSearchCity}/>
         <input type="submit" className="search-button" value="Search" />
-        <button className="current-location-button">Current Location</button>
       </form>
 
     <WeatherData data={weatherInfo} />
--- src/WeatherData.js
+++ src/WeatherData.js
@@ -2,6 +2,7 @@ import React from "react";
 import CurrentDate from "./CurrentDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherData.css"
 
 
 export default function WeatherData (props){
@@ -9,55 +10,36 @@ return (
   <div className="WeatherData">
 
       <section className="current-location">
-        <div className="container">
+        <div className="container transparency">
           <h2 className="searched-city">{props.data.city}</h2>
 
           <div className="row">
-            <div className="card col-3 transparency">
+            <div className="card col-4 transparency">
               <h3> Date and Time</h3>
               <div className="card-body">
                 <p className="card-text"><CurrentDate date={props.data.date} /></p>
               </div>
             </div>
 
-            <div className="card col-6 transparency">
+            <div className="card col-8 transparency">
               <h3>Temperature</h3>
               <div className="row">
                   <div className="card-body col-3 float-left">
                     <WeatherIcon code={props.data.icon} />
                   </div>
 
+                <div className="card-body float-left">
                 <WeatherTemperature celsius={props.data.temperature} />
-              
+                </div>
 
                 <div className="card-body float-left">
                   <ul className="temperature-description-box">
                     <li className="card-text">{props.data.description}</li>
                     <li className="card-text">Humidity: {props.data.humidity}%</li>
-                    <li className="card-text">
-                      Wind Speed: {props.data.windspeed} KM/h
-                    </li>
+                    <li className="card-text">Wind Speed: {props.data.windspeed} KM/H</li>
                   </ul>
                 </div>
               </div>
-            </div>
-            <div className="card col-3 transparency">
-              <div className="card-header">
-                <h4>Sunrise:</h4>
-              </div>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item transparency">
-                  {props.data.sunrise}
-                </li>
-              </ul>
-              <div className="card-header">
-                <h4>Sunset:</h4>
-              </div>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item transparency">
-                  {props.data.sunset}
-                </li>
-              </ul>
             </div>
           </div>
         </div>
