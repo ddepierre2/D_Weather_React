a9af7e5fc8df2d0ce47acf681f973b1dd29f5290
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-03-31 14:25:15 -0400

    Created working search bar

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,8 @@ import WeatherData from "./WeatherData";
 import "./Weather.css";
 
 export default function Weather(props) {
-  let [weatherInfo, setWeatherInfo] = useState ({ready:false});
+  const [weatherInfo, setWeatherInfo] = useState ({ready:false});
+  const [city, setCity] = useState (props.defaultCity);
 
 function axiosCall (response) {
   setWeatherInfo({
@@ -20,12 +21,27 @@ function axiosCall (response) {
     sunset: "19:10"
   })};
 
+  function search(){
+    const apiKey = "ed8ab9018735ed237ff0af3c6f9509f3"
+  let apiUnits = "metric"
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${apiUnits}`
+  axios.get(apiUrl).then(axiosCall);
+  }
+
+  function searchCity(event){
+event.preventDefault();
+search();
+  }
+
+function handleSearchCity(event){
+setCity(event.target.value);
+}
+
   if (weatherInfo.ready){
-  
    return ( 
      <div className="Weather">
-        <form className="location-form">
-        <input type="search" placeholder="Enter your city" autoComplete="off" />
+        <form className="location-form" onSubmit={searchCity}>
+        <input type="search" placeholder="Enter your city" autoComplete="off" onChange={handleSearchCity}/>
         <input type="submit" className="search-button" value="Search" />
         <button className="current-location-button">Current Location</button>
       </form>
@@ -35,12 +51,8 @@ function axiosCall (response) {
     </div>
   );
    } else{
-
-  const apiKey = "ed8ab9018735ed237ff0af3c6f9509f3"
-  let apiUnits = "metric"
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${apiUnits}`
-  axios.get(apiUrl).then(axiosCall);
-  return(<p>Loading</p>);
+     search();
+     return "Loading...";
    }
 }
 
