37303af27c74d2d90d4dd24188e488687c52a781
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-03-31 12:56:39 -0400

    Created a setWeather state

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ export default function App() {
    return ( <div className="App">
      <div className="container"> 
      <h1 className="title">Weather of the Day</h1>
-     <Weather />
+     <Weather defaultCity= "Montreal" />
      
-    <footer><a href="https://github.com/ddepierre2/D_Weather_React" target="_blank" 
-    rel="noreferrer" class="open-source-info">Open Source</a> code created by Dana De Pierre</footer>
+    <footer><a href="https://github.com/ddepierre2/D_Weather_React" target="_blank" rel="noreferrer" className="open-source-info">Open Source</a> code created by Dana De Pierre</footer>
     </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,41 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
+export default function Weather(props) {
+  let [weatherInfo, setWeatherInfo] = useState ({ready:false});
 
-
-export default function Weather() {
-  let Weather = {
-    city: "Montreal",
+function axiosCall (response) {
+  setWeatherInfo({
+    ready: true,
+    city: response.data.name,
     date: "Thursday, March 5, 2021",
     time: "15:32",
-    temperature: 25,
-    image: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-    description: "Overcast",
-    humidity: 2,
-    windspeed: 15,
+    temperature: Math.round(response.data.main.temp),
+    image: `http://openweathermap.org/img/wn/10n@2x.png`,
+    description: response.data.weather[0].description,
+    humidity: Math.round(response.data.main.humidity),
+    windspeed: Math.round(response.data.wind.speed),
     sunrise: "6:20",
     sunset: "19:10"
-  };
+  })};
+
+  if (weatherInfo.ready){
+  
    return ( 
    
   <div className="Weather">
         <form className="location-form">
         <input type="search" placeholder="Enter your city" autoComplete="off" />
-        <input type="submit" class="search-button" value="Search" />
+        <input type="submit" className="search-button" value="Search" />
         <button className="current-location-button">Current Location</button>
       </form>
 
       <section className="current-location">
         <div className="container">
-          <h2 className="searched-city">{Weather.city}</h2>
+          <h2 className="searched-city">{weatherInfo.city}</h2>
 
           <div className="row">
             <div className="card col-3 transparency">
               <h3> Date and Time</h3>
               <div className="card-body">
-                <p className="card-text">{Weather.date}</p>
-                <p className="card-text">{Weather.time}</p>
+                <p className="card-text">{weatherInfo.date}</p>
+                <p className="card-text">{weatherInfo.time}</p>
               </div>
             </div>
 
@@ -43,20 +48,20 @@ export default function Weather() {
               <h3>Temperature</h3>
               <div className="row">
                 <div className="card-body col-3 float-left">
-                  <img src={Weather.image} alt={Weather.description} />
+                  <img src={weatherInfo.image} alt={weatherInfo.description} />
                 </div>
 
                 <div className="card-body col-4 float-left">
-                  <strong className="now-temp">{Weather.temperature}</strong>
+                  <strong className="now-temp">{weatherInfo.temperature}</strong>
                   <a href="/">°C</a> | <a href="/">°F</a>
                 </div>
 
                 <div className="card-body float-left">
                   <ul className="temperature-description-box">
-                    <li className="card-text">{Weather.description}</li>
-                    <li className="card-text">Humidity: {Weather.humidity}%</li>
+                    <li className="card-text">{weatherInfo.description}</li>
+                    <li className="card-text">Humidity: {weatherInfo.humidity}%</li>
                     <li className="card-text">
-                      Wind Speed: {Weather.windspeed} KM/h
+                      Wind Speed: {weatherInfo.windspeed} KM/h
                     </li>
                   </ul>
                 </div>
@@ -68,7 +73,7 @@ export default function Weather() {
               </div>
               <ul className="list-group list-group-flush">
                 <li className="list-group-item transparency">
-                  {Weather.sunrise}
+                  {weatherInfo.sunrise}
                 </li>
               </ul>
               <div className="card-header">
@@ -76,7 +81,7 @@ export default function Weather() {
               </div>
               <ul className="list-group list-group-flush">
                 <li className="list-group-item transparency">
-                  {Weather.sunset}
+                  {weatherInfo.sunset}
                 </li>
               </ul>
             </div>
@@ -86,5 +91,13 @@ export default function Weather() {
   
     </div>
   );
+   } else{
+
+  const apiKey = "ed8ab9018735ed237ff0af3c6f9509f3"
+  let apiUnits = "metric"
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${apiUnits}`
+  axios.get(apiUrl).then(axiosCall);
+  return(<p>Loading</p>);
+   }
 }
 
