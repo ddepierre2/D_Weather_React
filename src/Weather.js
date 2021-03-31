538de738d11c5c39e83882ecedd9bdec6915e484
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-03-31 14:08:51 -0400

    Created WeatherData component

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import CurrentDate from "./CurrentDate";
+import WeatherData from "./WeatherData";
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -23,71 +23,15 @@ function axiosCall (response) {
   if (weatherInfo.ready){
   
    return ( 
-   
-  <div className="Weather">
+     <div className="Weather">
         <form className="location-form">
         <input type="search" placeholder="Enter your city" autoComplete="off" />
         <input type="submit" className="search-button" value="Search" />
         <button className="current-location-button">Current Location</button>
       </form>
 
-      <section className="current-location">
-        <div className="container">
-          <h2 className="searched-city">{weatherInfo.city}</h2>
+    <WeatherData data={weatherInfo} />
 
-          <div className="row">
-            <div className="card col-3 transparency">
-              <h3> Date and Time</h3>
-              <div className="card-body">
-                <p className="card-text"><CurrentDate date={weatherInfo.date} /></p>
-              </div>
-            </div>
-
-            <div className="card col-6 transparency">
-              <h3>Temperature</h3>
-              <div className="row">
-                <div className="card-body col-3 float-left">
-                  <img src={weatherInfo.image} alt={weatherInfo.description} />
-                </div>
-
-                <div className="card-body col-4 float-left">
-                  <strong className="now-temp">{weatherInfo.temperature}</strong>
-                  <a href="/">°C</a> | <a href="/">°F</a>
-                </div>
-
-                <div className="card-body float-left">
-                  <ul className="temperature-description-box">
-                    <li className="card-text">{weatherInfo.description}</li>
-                    <li className="card-text">Humidity: {weatherInfo.humidity}%</li>
-                    <li className="card-text">
-                      Wind Speed: {weatherInfo.windspeed} KM/h
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="card col-3 transparency">
-              <div className="card-header">
-                <h4>Sunrise:</h4>
-              </div>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item transparency">
-                  {weatherInfo.sunrise}
-                </li>
-              </ul>
-              <div className="card-header">
-                <h4>Sunset:</h4>
-              </div>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item transparency">
-                  {weatherInfo.sunset}
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </section>
-  
     </div>
   );
    } else{
---
+++ src/WeatherData.js
@@ -0,0 +1,69 @@
+import React from "react";
+import CurrentDate from "./CurrentDate";
+
+
+
+export default function WeatherData (props){
+return (
+  <div className="WeatherData">
+
+      <section className="current-location">
+        <div className="container">
+          <h2 className="searched-city">{props.data.city}</h2>
+
+          <div className="row">
+            <div className="card col-3 transparency">
+              <h3> Date and Time</h3>
+              <div className="card-body">
+                <p className="card-text"><CurrentDate date={props.data.date} /></p>
+              </div>
+            </div>
+
+            <div className="card col-6 transparency">
+              <h3>Temperature</h3>
+              <div className="row">
+                <div className="card-body col-3 float-left">
+                  <img src={props.data.image} alt={props.data.description} />
+                </div>
+
+                <div className="card-body col-4 float-left">
+                  <strong className="now-temp">{props.data.temperature}</strong>
+                  <a href="/">°C</a> | <a href="/">°F</a>
+                </div>
+
+                <div className="card-body float-left">
+                  <ul className="temperature-description-box">
+                    <li className="card-text">{props.data.description}</li>
+                    <li className="card-text">Humidity: {props.data.humidity}%</li>
+                    <li className="card-text">
+                      Wind Speed: {props.data.windspeed} KM/h
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="card col-3 transparency">
+              <div className="card-header">
+                <h4>Sunrise:</h4>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item transparency">
+                  {props.data.sunrise}
+                </li>
+              </ul>
+              <div className="card-header">
+                <h4>Sunset:</h4>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item transparency">
+                  {props.data.sunset}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+  
+    </div>
+)
+}
