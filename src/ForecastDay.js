e1d0f33846731ea8dfca30bb6c2bd124b5ebb1c5
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-04-01 14:39:41 -0400

    fixed icon size bug

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -11,26 +11,20 @@ export default function ForecastDay(props){
       let temperature = Math.round(props.data.temp.min);
       return `${temperature}°`;
     }
-function day(){
-  let date = new Date (props.data.dt * 1000);
-  let day = date.getDay();
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
-  return days[day];
-
-
-}
-
-    return(
-   <div className="ForecastDay">
-   <div className="forecastDate">{day()}</div> 
-              <WeatherIcon code={props.data.weather[0].icon} size={25} />
-              <div className="forecastTemp">
-                <span className="forecast-temp-max"> {maxTemperature()} | </span> 
-                <span className="forecast-temp-min">{minTemperature()}</span>
-              </div>
-
-
-  </div>
-  );
+    function day(){
+      let date = new Date (props.data.dt * 1000);
+      let day = date.getDay();
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+      return days[day];
+    }
+      return(
+      <div className="ForecastDay">
+      <div className="forecastDate">{day()}</div> 
+                  <WeatherIcon code={props.data.weather[0].icon} size={30} />
+                  <div className="forecastTemp">
+                    <span className="forecast-temp-max"> {maxTemperature()} | </span> 
+                    <span className="forecast-temp-min">{minTemperature()}</span>
+                  </div>
+      </div>
+      );
 }
--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -22,24 +22,12 @@ export default function WeatherIcon (props){
     "50d": "FOG",
     "50n": "FOG"
   }
-if (props.code === "01d"){
-  return (
- <ReactAnimatedWeather
-    icon={codeMapping[props.code]}
-    color="#321970"
-    size={64}
-    animate={true}
-  />
-  );
-} else {
- return (
- <ReactAnimatedWeather
-    icon={codeMapping[props.code]}
-    color="#321970"
-    size={props.size}
-    animate={true}
-  />
-  );
-
-}
+    return (
+      <ReactAnimatedWeather
+        icon={codeMapping[props.code]}
+        color="#321970"
+        size={props.size}
+        animate={true}
+      />
+    );   
 }
