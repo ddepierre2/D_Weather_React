427cc85a97e7dda0ab0455a7057b96c90a6aa4ea
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-04-01 14:30:15 -0400

    Updated search code in Forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -8,14 +8,24 @@ export default function Forecast (props){
   let [loaded, setLoaded] = useState (false);
   let [forecastData, setForecastData] = useState (null);
   
-useEffect(() => {
-setLoaded(false);
-}, [props.coordinates]);
+  useEffect(() => {
+  setLoaded(false);
+  }, [props.coordinates]);
 
   function showForecast(response){
     setForecastData(response.data.daily);
     setLoaded(true);
   }
+
+  function searchUrl(){
+    const apiKey = "ed8ab9018735ed237ff0af3c6f9509f3";
+    let apiUnits = "metric";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${apiUnits}`
+    axios.get(apiUrl).then(showForecast);
+  }
+  
   if (loaded){
     return (
       <div className="Forecast">
@@ -35,12 +45,7 @@ setLoaded(false);
       </div>
     );
   } else {
-      const apiKey = "ed8ab9018735ed237ff0af3c6f9509f3";
-      let apiUnits = "metric";
-      let latitude = props.coordinates.lat;
-      let longitude = props.coordinates.lon;
-      let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${apiUnits}`
-      axios.get(apiUrl).then(showForecast);
+    searchUrl(); 
     return null;
   }
 }
